4a0d7c0dccac1bab0108f8fd5ed92e3957418046
Florian Schulze <mail@florian-schulze.net>
2024-02-16 10:29:26 +0100

    Adjust test setup to changes in devpi-server.

--- test_devpi_mirror_index_acl.py
+++ test_devpi_mirror_index_acl.py
@@ -1,26 +1,33 @@
-from test_devpi_server.conftest import gentmp
-from test_devpi_server.conftest import httpget
-from test_devpi_server.conftest import makemapp
-from test_devpi_server.conftest import maketestapp
-from test_devpi_server.conftest import makexom
-from test_devpi_server.conftest import mapp
-from test_devpi_server.conftest import pypiurls
-from test_devpi_server.conftest import storage_info
-from test_devpi_server.conftest import testapp
+from devpi_common.metadata import parse_version
+from devpi_server import __version__ as _devpi_server_version
 import pytest
 
+devpi_server_version = parse_version(_devpi_server_version)
+pytestmark = [pytest.mark.notransaction]
 
-(
-    gentmp,
-    httpget,
-    makemapp,
-    maketestapp,
-    makexom,
-    mapp,
-    pypiurls,
-    storage_info,
-    testapp,
-)
+if devpi_server_version < parse_version("6.9.3dev"):
+    from test_devpi_server.conftest import gentmp
+    from test_devpi_server.conftest import httpget
+    from test_devpi_server.conftest import makemapp
+    from test_devpi_server.conftest import maketestapp
+    from test_devpi_server.conftest import makexom
+    from test_devpi_server.conftest import mapp
+    from test_devpi_server.conftest import pypiurls
+    from test_devpi_server.conftest import storage_info
+    from test_devpi_server.conftest import testapp
+    (
+        gentmp,
+        httpget,
+        makemapp,
+        maketestapp,
+        makexom,
+        mapp,
+        pypiurls,
+        storage_info,
+        testapp,
+    )
+else:
+    pytest_plugins = ["pytest_devpi_server", "test_devpi_server.plugin"]
 
 
 @pytest.fixture
