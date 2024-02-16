a699edf0073d40475adf58e25fd1cb491e8cfa84
Florian Schulze <mail@florian-schulze.net>
2024-02-16 11:22:28 +0100

    Add README and some configuration tests.

--- test_devpi_mirror_index_acl.py
+++ test_devpi_mirror_index_acl.py
@@ -39,16 +39,48 @@ def xom(request, makexom):
 
 
 def test_mirror_index_acl(mapp, testapp, xom):
-    app = testapp.app
-    while 1:
-        registry = getattr(app, "registry", None)
-        if registry is not None:
-            break
-        app = app.app
     xom.config.args.acl_mirror_index_create = "root"
+    mapp.login_root()
+    mapp.create_index("root/mirror", indexconfig=dict(
+        type="mirror"))
     mapp.create_and_login_user("user")
     mapp.create_index("user/mirror", code=403, indexconfig=dict(
         type="mirror"))
     xom.config.args.acl_mirror_index_create = None
     mapp.create_index("user/mirror", indexconfig=dict(
+        type="mirror"))
+    xom.config.args.acl_mirror_index_create = "user"
+    mapp.create_index("user/mirror2", indexconfig=dict(
+        type="mirror"))
+
+
+def test_environment_config(makexom, mapp, monkeypatch):
+    import devpi_mirror_index_acl
+    xom = makexom(plugins=[
+        (devpi_mirror_index_acl, None)])
+    assert xom.config.args.acl_mirror_index_create is None
+    monkeypatch.setenv("DEVPISERVER_ACL_MIRROR_INDEX_CREATE", "root")
+    xom = makexom(plugins=[
+        (devpi_mirror_index_acl, None)])
+    assert xom.config.args.acl_mirror_index_create == "root"
+    mapp.login_root()
+    mapp.create_index("root/mirror", indexconfig=dict(
+        type="mirror"))
+
+
+def test_config_file(makexom, mapp, tmp_path):
+    import devpi_mirror_index_acl
+    import textwrap
+    path = tmp_path.joinpath('devpi.yml')
+    path.write_text(textwrap.dedent('''
+        devpi-server:
+          acl-mirror-index-create:
+            - "root"
+    '''))
+    xom = makexom(
+        opts=('-c', str(path)),
+        plugins=[(devpi_mirror_index_acl, None)])
+    assert xom.config.args.acl_mirror_index_create == ["root"]
+    mapp.login_root()
+    mapp.create_index("root/mirror", indexconfig=dict(
         type="mirror"))
